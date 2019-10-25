b06a954b4fbb4f0747107dce0fba568eb73f43c4
habbyge <habbyge@tencent.com>
2019-10-25 22:02:31 +0800

    验证final class、final method的例子

--- src/main/java/com/lxyx/habbyge/TestProxy.java
+++ src/main/java/com/lxyx/habbyge/TestProxy.java
@@ -1,12 +1,14 @@
 package com.lxyx.habbyge;
 
-public class TestProxy {
+// fixme: 这里的例子是：final，一样能够对其进行动态代理劫持
+public final class TestProxy {
 
     public void foo() {
         System.out.println("_LXYX_ TestProxy, foo()");
     }
 
-    public String foo1(String name) {
+    // fixme: 这里的例子是：final，一样能够对其进行动态代理劫持
+    public final String foo1(String name) {
         System.out.println("_LXYX_ TestProxy, foo1(): " + name);
         return name;
     }
